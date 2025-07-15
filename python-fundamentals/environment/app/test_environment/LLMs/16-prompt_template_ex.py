86571b70ca3a15b7235a28ef1786171f5f0b886a
Santos Ortiz <santos95ortiz@gmail.com>
2025-07-14 20:10:22 -0700

    Prompt Templates practice

---
+++ python-fundamentals/environment/app/test_environment/LLMs/16-prompt_template_ex.py
@@ -0,0 +1,75 @@
+# import required libraries
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
+from langchain_core.prompts import ChatPromptTemplate 
+from langchain_core.prompts import ChatPromptTemplate
+
+# define parameters
+base_url = "https://integrate.api.nvidia.com/v1"
+api_key = "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"
+model = 'meta/llama-3.1-8b-instruct'
+
+# create the llm langchain instance
+llm = ChatNVIDIA(
+    base_url=base_url, 
+    api_key=api_key,
+    model=model, 
+    temperature=0
+    )
+
+# General task - capture 3 llm-related tasks into a prompt template and apply them to a list of statements
+# 1 - Sentimental analysis - Ascertain the overall sentiment of a given text
+# 2 - Main topic identification - identify the main topic for a given text
+# 3 - Followup question generation = Generates an appropiate followup question to clarify some aspect of a text
+
+# list of statements to work with
+statements = [
+    "I had a fantastic time hiking up the mountain yesterday.",
+    "The new restaurant downtown serves delicious vegetarian dishes.",
+    "I am feeling quite stressed about the upcoming project deadline.",
+    "Watching the sunset at the beach was a calming experience.",
+    "I recently started reading a fascinating book about space exploration."
+]
+
+
+    
+
+# Define the templates 
+# sentimental analysis template
+sentimental_analysis_temp = ChatPromptTemplate.from_template("""Perform a sentimental analysis of the next piece of a text \
+by specifying if the text is 'positive' or 'negative'. The result has to be the single word if is 'Positive' or 'Negative'. \
+Avoid any other sentence.This is the text: {statement}""")
+
+main_topic_temp = ChatPromptTemplate.from_template("""Identify accurately and conscisely the main topic of a text. Only return the \
+the main topic description and avoid any other sentences. This is the text: {statement}""")
+
+followup_temp = ChatPromptTemplate.from_template("""Generate an interesting and appropiate followup question in away that allows to clarify some aspect of a text. \
+Returns as result only the generated question, without any other sentece. This is the text: {statement}""")
+
+# create the prompt 
+sentimental_analysis_prompts = [sentimental_analysis_temp.invoke({"statement":statement}) for statement in statements]
+
+main_topic_prompts = [main_topic_temp.invoke({"statement":statement}) for statement in statements]
+
+followup_prompts = [followup_temp.invoke({"statement":statement}) for statement in statements]
+
+
+# define the response list
+sentimental_responses = []
+main_topic_responses = []
+followup_responses = []
+
+# request the llms 
+sentimental_responses = llm.batch(sentimental_analysis_prompts)
+
+main_topic_responses = llm.batch(main_topic_prompts)
+
+followup_responses = llm.batch(followup_prompts)
+
+for statement, sentimental_response, main_topic_response, followup_response in zip(statements, sentimental_responses, main_topic_responses, followup_responses):
+    
+    print(
+        f"Statement: {statement}\n"
+        f"Overall Sentiment: {sentimental_response.content}\n"
+        f"Main Topic: {main_topic_response.content}\n"
+        f"Followup question: {followup_response.content}\n"
+        )
---
+++ python-fundamentals/environment/app/test_environment/LLMs/16-prompts_templates.py
@@ -0,0 +1,91 @@
+# prompts templates to capture llM-RELATED tasks into prompts templates
+# in that way create reusable prompt templates
+# perform a variety llm powered tasks on a collection of text examples 
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
+from langchain_core.prompts import ChatPromptTemplate 
+
+base_url = "https://integrate.api.nvidia.com/v1"
+api_key = "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"
+model = 'meta/llama-3.1-8b-instruct'
+
+llm = ChatNVIDIA(
+    base_url=base_url, 
+    api_key=api_key,
+    model=model, 
+    temperature=0
+    )
+
+# emulate the reuse code of functions functionality - thats what we want with prompts template
+# be able to reuse prompts with different inputs 
+
+# test with one off taks
+one_off_prompt = "Translate the following from English to Spanish: 'Today is a good day.'"
+
+def print_prompt(prompt):
+    print(f"This is my prompt: {prompt}")
+
+def print_response(response):
+    print(f"This is the response: {response}")
+
+
+# perform a llm request
+print_prompt(one_off_prompt)
+response = llm.invoke(one_off_prompt).content 
+
+print_response(response)
+
+## so we can make that prompt into a reusable one 
+# we do that by abstracting parts of the prompts into arguments 
+# in that way we get a prompt that can be used with arbitraries inputs
+
+# this functions creates a prompt template that capture the functionality of translating and english statement into a spanish
+def translate_from_eng_to_sp(en_statement):
+    return f"Translate the following from English to Spanish: {en_statement}"
+
+# set a list of statements to test
+
+en_statements = [
+    'Today is a good day.'
+    'Tomorrow will be even better.'
+]
+
+# create a list of prompts with the template function
+prompts = [translate_from_eng_to_sp(en_statement) for en_statement in en_statements]
+
+
+# test - perform batch statements
+translations = llm.batch(prompts)
+
+# print the translations
+for translation in translations:
+    print(translation.content)
+
+
+# create a template that are able to to the same but adding from which language to a lenguage
+def translate(from_lang, to_lang, statement):
+    return f"Translate the following from {from_lang} to {to_lang}. Provide only the translated text: {statement}"
+
+print(llm.invoke(translate('English', 'Portugues', 'I am become death the destroyer of worlds!')))
+
+
+# use the prompt template from the ChatPromptTemplate package from langchain
+from langchain_core.prompts import ChatPromptTemplate
+
+# creates a template 
+en_to_sp_template = ChatPromptTemplate.from_template("""Translate the following from English to Spanish. \
+Provide only the translated text: '{english_statement}'""")
+
+# get the prompt from the template
+prompt = en_to_sp_template.invoke("Now I am become death, the destroyer of worlds!")
+
+print(prompt)
+
+print("This is the response: ", llm.invoke(prompt).content)
+
+# the best practices for templates when we create them is to use a dict to map
+# the placeholders values with the content, the above only pass whe content because is a single value
+# for more than one values is necessary to pass dicts. but as best practice even 
+# is it only one pass a dict
+prompt = en_to_sp_template.invoke({"english_statement":"Now I am become death, the destroyer of worlds!"})
+
+print(prompt)
